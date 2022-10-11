0227e2cc6f9ed0227a3528febda9ee57d2c84582
Dr. Peter Dillinger <peter.dillinger@srh.de>
2022-10-11 15:27:05 +0200

    erstes GUI Fenster mit Text und Button

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,8 +1,30 @@
 package controller;
 
+import view.MainView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainController {
+    private boolean toggle = false;
+
+    public MainController(MainView mainView) {
+        mainView.setTextMitte("Hier hat der Controller die Kontrolle!");
+
+        mainView.setKlichMichButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (toggle)
+                    mainView.setTextMitte("Yeah, es hat geklickt!");
+                else
+                    mainView.setTextMitte("Willst du vielleicht mal klicken?");
+                toggle = !toggle;
+            }
+        });
+
+    }
+
     public static void main(String[] args) {
-
-
+        new MainController( new MainView() );
     }
 }
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,4 +1,47 @@
 package view;
 
-public class MainView {
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class MainView extends JFrame {
+
+    private final JLabel textMitte;
+    private final JButton klichMichButton;
+
+    public MainView() {
+        setTitle("Mein erstes GUI Programm");
+        setSize(400, 200);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setLayout( new BorderLayout() );
+
+        JLabel textOben = new JLabel("Hier steht hilfreicher Text!");
+        textOben.setBorder( new LineBorder(Color.BLACK) );
+        textOben.setBackground( Color.CYAN );
+        textOben.setOpaque(true);
+
+        textMitte = new JLabel("Das Zentrum");
+
+        add( textOben, BorderLayout.NORTH );
+        add( textMitte, BorderLayout.CENTER );
+
+        klichMichButton = new JButton("Klick mich");
+        add( klichMichButton, BorderLayout.SOUTH );
+
+        setVisible(true);
+    }
+
+    public void setKlichMichButtonListener(ActionListener listener) {
+        klichMichButton.addActionListener(listener);
+    }
+
+    public void setTextMitte(String text) {
+        textMitte.setText(text);
+    }
+
+    public String getTextMitte() {
+        return textMitte.getText();
+    }
 }
