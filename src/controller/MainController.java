8ee859fe3ad0c92813ea77f67e996b2d60c5b989
Zwitscherhansi <95081892+Zwitscherhansi@users.noreply.github.com>
2022-10-11 14:32:25 +0200

    Irgendwas was lustig ist !

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,6 +3,7 @@ package controller;
 public class MainController {
     public static void main(String[] args) {
         System.out.println("Sollte geklappt haben?");
+        System.out.println("Irgendwas was lustig ist ");
 
     }
 }
